d1b92aba7d0bb226f8e6b4b2e52471491fa9b71c
Sandy Barnabas <sbarnabas@gmail.com>
2015-11-07 12:33:20 -0500

    formatting
    reformatted a bunch of python files

--- main/api/fields.py
+++ main/api/fields.py
@@ -7,41 +7,48 @@ from flask.ext.restful.fields import *
 
 
 class BlobKey(fields.Raw):
-  def format(self, value):
-    return urllib.quote(str(value))
+
+    def format(self, value):
+        return urllib.quote(str(value))
 
 
 class Blob(fields.Raw):
-  def format(self, value):
-    return repr(value)
+
+    def format(self, value):
+        return repr(value)
 
 
 class DateTime(fields.DateTime):
-  def format(self, value):
-    return value.isoformat()
+
+    def format(self, value):
+        return value.isoformat()
 
 
 class GeoPt(fields.Raw):
-  def format(self, value):
-    return '%s,%s' % (value.lat, value.lon)
+
+    def format(self, value):
+        return '%s,%s' % (value.lat, value.lon)
 
 
 class Id(fields.Raw):
-  def output(self, key, obj):
-    try:
-      value = getattr(obj, 'key', None).id()
-      return super(Id, self).output(key, {'id': value})
-    except AttributeError:
-      return None
+
+    def output(self, key, obj):
+        try:
+            value = getattr(obj, 'key', None).id()
+            return super(Id, self).output(key, {'id': value})
+        except AttributeError:
+            return None
 
 
 class Integer(fields.Integer):
-  def format(self, value):
-    if value > 9007199254740992 or value < -9007199254740992:
-      return str(value)
-    return value
+
+    def format(self, value):
+        if value > 9007199254740992 or value < -9007199254740992:
+            return str(value)
+        return value
 
 
 class Key(fields.Raw):
-  def format(self, value):
-    return value.urlsafe()
+
+    def format(self, value):
+        return value.urlsafe()
--- main/control/error.py
+++ main/control/error.py
@@ -19,25 +19,25 @@ from main import app
 @app.errorhandler(418)  # I'm a Teapot
 @app.errorhandler(500)  # Internal Server Error
 def error_handler(e):
-  logging.exception(e)
-  try:
-    e.code
-  except AttributeError:
-    e.code = 500
-    e.name = 'Internal Server Error'
+    logging.exception(e)
+    try:
+        e.code
+    except AttributeError:
+        e.code = 500
+        e.name = 'Internal Server Error'
 
-  if flask.request.path.startswith('/api/'):
-    return helpers.handle_error(e)
+    if flask.request.path.startswith('/api/'):
+        return helpers.handle_error(e)
 
-  return flask.render_template(
-      'error.html',
-      title='Error %d (%s)!!1' % (e.code, e.name),
-      html_class='error-page',
-      error=e,
+    return flask.render_template(
+        'error.html',
+        title='Error %d (%s)!!1' % (e.code, e.name),
+        html_class='error-page',
+        error=e,
     ), e.code
 
 
 if config.PRODUCTION:
-  @app.errorhandler(Exception)
-  def production_error_handler(e):
-    return error_handler(e)
+    @app.errorhandler(Exception)
+    def production_error_handler(e):
+        return error_handler(e)
